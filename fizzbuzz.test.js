5caa6875b6742db313d656a23292db04a6145af6
jcfernandez <jcfernandez@theworkshop.com>
2021-03-26 15:12:15 +0100

    step 6: should print fizzbuzz if the given value is multiple of 3 and 5

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -30,4 +30,10 @@ describe("fizzbuzz", () => {
     const result = fizzbuzz(10);
     expect(expected).toBe(result);
   });
+
+  test("should print fizzbuzz if the given value is multiple of 3 and 5", () => {
+    const expected = "fizzbuzz";
+    const result = fizzbuzz(15);
+    expect(expected).toBe(result);
+  });
 });
